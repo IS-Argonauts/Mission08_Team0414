4e15678024c3cd700382a5f2c9c28d239dfc8e2e
Yuto001 <yuto.t1234@gmail.com>
2025-02-24 15:46:17 -0700

    I added some code to make database

---
+++ Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission08_Team0414.Models
+{
+	public class Task()
+	{
+		[Key]
+		[Required]
+		public int TaskId { get; set; }
+		public string TaskName { get; set; }
+		public string DueDate {  get; set; }
+		public int Quadrant {  get; set; }
+		public string Category {  get; set; }
+		public bool Completed { get; set; }
+	}
+}
